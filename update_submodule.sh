99eb037806e3bd533f182c3485681bf15e81bc5d
mog <mog@rldn.net>
2014-02-09 19:03:03 -0600

    this makes it work from any path

--- update_submodule.sh
+++ update_submodule.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+Path=`pwd`
+cd  `dirname $0`
 git submodule init
 git submodule update
 cd meatstand_common
 git pull http://dev.meatstand.com/mog/meatstand_common.git master
-cd ..
-
+cd "${Path}"
