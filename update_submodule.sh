cdb53292035ac4d0f1c5136a4f5759331d88f1b2
Matthew O'Gorman <mog@rldn.net>
2014-06-06 23:13:31 -0500

    new update submodule from pig

--- update_submodule.sh
+++ update_submodule.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 Path=`pwd`
 cd  `dirname $0`
-git submodule init
-git submodule update
-cd meatstand_common
-git pull http://dev.meatstand.com/meatstand/meatstand_common.git master
+git submodule update --init --recursive
 cd "${Path}"
