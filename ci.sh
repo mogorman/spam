8543c54d5275f5239fb9ec103c5014d6851278aa
Matthew O'Gorman <mog@rldn.net>
2015-03-31 22:11:44 -0500

    updated ci script

--- ci.sh
+++ ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #this is where continuous integration script will live
+Project="spam"
+
 copy_files() {
     local new_copy="$1"
     local old_copy="$2"
@@ -9,7 +11,7 @@ copy_files() {
     else
 	echo File changed make copies.
 	cp "$1" "$2"
-	cp "$1" "/home/gitlab_ci_runner/artifacts/spam/build/"`git rev-parse --short HEAD`_"$1"
+	cp "$1" "/home/gitlab_ci_runner/artifacts/${Project}/build/"`git rev-parse --short HEAD`_"$1"
     fi    
 }
 
@@ -23,25 +25,25 @@ make CUSTOMPCB="/usr/src/pcb/src/pcb" \
 make CUSTOMPCB="/usr/src/pcb/src/pcb" \
      CUSTOMPCB_ARGS="--photo-mask-colour red \
      --photo-silk-colour white  --photo-plating  tinned" photos
-ls black_board.png board.png bom order xy schematic.png schematic.pdf gerbers/spam_make.bottom.gbr > /dev/null 2>&1
+ls black_board.png board.png bom order xy schematic.png schematic.pdf gerbers/${Project}_make.bottom.gbr > /dev/null 2>&1
 if [ "$?" != "0" ]; then
     echo "I failed to create all the files I should have. build failed"
     exit 1
 fi
-New_spam=`git diff HEAD^ HEAD -- spam.pcb|wc -l`
-New_schematic=`git diff HEAD^ HEAD -- spam.sch|wc -l`
+New_project=`git diff HEAD^ HEAD -- ${Project}.pcb|wc -l`
+New_schematic=`git diff HEAD^ HEAD -- ${Project}.sch|wc -l`
 
-if [ "$New_spam" == "0" ]; then
-    echo "No changes to spam don't bother saving image."
+if [ "$New_project" == "0" ]; then
+    echo "No changes to ${Project} don't bother saving image."
 else
-    copy_files board.png ~/artifacts/spam/board.png
+    copy_files board.png ~/artifacts/${Project}/board.png
     File=`ls *.zip|grep -v oshpark`
     cp "${File}" gerbers.zip
-    copy_files gerbers.zip ~/artifacts/spam/gerbers.zip
+    copy_files gerbers.zip ~/artifacts/${Project}/gerbers.zip
 fi
 
 if [ "$New_schematic" == "0" ]; then
     echo "No changes to schematic don't bother saving the image."
 else
-    copy_files schematic.png ~/artifacts/spam/schematic.png
+    copy_files schematic.png ~/artifacts/${Project}/schematic.png
 fi
